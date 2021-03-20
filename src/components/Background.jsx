e0f6d113d07a7ca4a8e2cf846ae16d41ecc3ffa5
Carles <carles.gstocks@gmail.com>
2021-03-20 09:23:41 +0100

    Background with Mouuse working AWESOME

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -1,8 +1,5 @@
-import React, { useRef, useEffect, useState, useContext, memo } from "react";
-import { useSpring } from "react-spring";
-import classnames from "classnames";
-import SVG from "react-inlinesvg";
-
+import React, { useRef, useEffect, useContext, memo } from "react";
+import { useSpring, animated } from "react-spring";
 // Icons
 import DesignPattern from "resources/patterns/designPattern.svg";
 import WebPattern from "resources/patterns/electronicPattern.svg";
@@ -10,14 +7,9 @@ import GamePattern from "resources/patterns/gamingPattern.svg";
 
 // Contexts
 import { Utils } from "contexts/Utils";
-import { Data } from "contexts/Data";
 import { isMobileOnly } from "react-device-detect";
 
 // Constants
-const COLLAPSE_NAVBAR_WIDTH = 1100;
-const MAX_FPS = 120;
-const FPS = 60;
-const DECELERATION = 10;
 const GRADIENTS = {
     web: ["#2192bf", "#02f8ab"],
     game: ["#f46b6b", "#f4a658"],
@@ -28,15 +20,15 @@ const PATTERNS = {
     game: { svg: GamePattern, tileSize: 200, tileSizeMobile: 150 },
     design: { svg: DesignPattern, tileSize: 300, tileSizeMobile: 200 },
 };
+const OPACITIES = {
+    web: 0.2,
+    game: 0.25,
+    design: 0.15,
+};
 
 const Background = memo(({ parent }) => {
     // Contexts
-    const { useForceUpdate, invlerp } = useContext(Utils);
-    const { positionRef, speedRef, motion, prevMotion } = useContext(Data);
-
-    // Force update
-    const forceUpdate = useForceUpdate();
-
+    const { invlerp } = useContext(Utils);
     // Save parent in a ref
     const parentRef = useRef(parent);
     useEffect(() => {
@@ -47,14 +39,26 @@ const Background = memo(({ parent }) => {
     //   BACKGROUND LOGIC
     // #################################################
 
-    // Current background & pattern
-    const currGradient = useRef("web");
-    const [currPattern, setCurrPattern] = useState("web");
+    // Current section
+    const section = useRef("web");
 
-    // Spring
-    const [{ background }, setGradient] = useSpring(() => ({
-        background: `linear-gradient(60deg, ${GRADIENTS[currGradient.current][0]} 0%, ${GRADIENTS[currGradient.current][1]} 100%)`,
+    // Background position spring
+    const [{ backgroundPosition }, setPosition] = useSpring(() => ({
+        backgroundPosition: "0px 0px",
+        config: { friction: 600, mass: 500 },
+    }));
+
+    // Background gradient spring
+    const [{ backgroundImage }, setGradient] = useSpring(() => ({
+        backgroundImage: `linear-gradient(60deg, ${GRADIENTS[section.current][0]} 0%, ${GRADIENTS[section.current][1]} 100%)`,
         config: { friction: 40 },
+    }));
+
+    // Background opacity spring
+    const [opacities, setOpacities] = useSpring(() => ({
+        opacityWeb: OPACITIES.web,
+        opacityGame: 0,
+        opacityDesign: 0,
     }));
 
     // Set te background gradient by one of its presets
@@ -62,66 +66,45 @@ const Background = memo(({ parent }) => {
         if (!(sectionName in GRADIENTS) || !(sectionName in PATTERNS)) return;
 
         // Set the background gradient
-        setGradient({ background: `linear-gradient(60deg, ${GRADIENTS[sectionName][0]} 0%, ${GRADIENTS[sectionName][1]} 100%)` });
-        currGradient.current = sectionName;
+        setGradient({ backgroundImage: `linear-gradient(60deg, ${GRADIENTS[sectionName][0]} 0%, ${GRADIENTS[sectionName][1]} 100%)` });
+        section.current = sectionName;
 
-        // Set the background pattern
-        setCurrPattern(sectionName);
-    };
-
-    // #################################################
-    //   RESIZE LOGIC
-    // #################################################
-
-    // Resize timeout
-    const resizeTimeout = useRef(null);
-
-    const onResize = () => {
-        if (resizeTimeout.current) clearTimeout(resizeTimeout.current);
-
-        resizeTimeout.current = setTimeout(forceUpdate, 500);
+        // Change pattern opacities
+        setOpacities({
+            opacityWeb: sectionName === "web" ? OPACITIES.web : 0,
+            opacityGame: sectionName === "game" ? OPACITIES.game : 0,
+            opacityDesign: sectionName === "design" ? OPACITIES.design : 0,
+        });
     };
 
     // #################################################
     //   ACCELEROMETER TILT
     // #################################################
 
-    // Current position
-    const [prevGradient, setPrevGradient] = useState(background.get());
-
-    // Current position
-    const [position, setPosition] = useState(positionRef.current);
-
-    // Current speed
-    const [{ speed }, setSpeed] = useSpring(() => ({ speed: speedRef.current }));
-
     // Handle device orientation change
     const onDeviceMotion = ({ rotationRate }) => {
         const { alpha, beta } = rotationRate;
 
-        // New motion
-        var newMotion = {
-            alpha: motion.current.alpha,
-            beta: motion.current.beta,
-        };
+        // Return if alpha or beta are undefined
+        if (!alpha || !beta) return;
 
-        // Update alpha
-        if (Math.abs(alpha) > 20) {
-            newMotion.alpha =
-                (Math.sign(motion.current.alpha) === Math.sign(alpha) || motion.current.alpha === 0) && Math.abs(motion.current.alpha) > Math.abs(alpha) ? motion.current.alpha : alpha;
-        }
+        // Normalize tilt [-1, 1]
+        const normX = invlerp(0, 100, alpha) * 2 - 1;
+        const normY = invlerp(0, 100, beta) * 2 - 1;
 
-        if (Math.abs(beta) > 20) {
-            newMotion.beta = (Math.sign(motion.current.beta) === Math.sign(beta) || motion.current.beta === 0) && Math.abs(motion.current.beta) > Math.abs(beta) ? motion.current.beta : beta;
-        }
+        // Current background position
+        const backgroundPos = backgroundPosition.get().replace(" ", "").split("px");
+        const backgroundCoords = { x: parseInt(backgroundPos[0]), y: parseInt(backgroundPos[1]) };
 
-        motion.current = newMotion;
+        console.log(rotationRate);
+        console.log(`Motion ${backgroundCoords.x - normX * 1000}px ${backgroundCoords.y - normY * 1000}px`);
+        setPosition({ backgroundPosition: `${backgroundCoords.x - normX * 1000}px ${backgroundCoords.y - normY * 1000}px` });
     };
 
     // Handle mouse move change
     const onMouseMove = (event) => {
         // Return while not in production
-        if (process.env.REACT_APP_DEBUGG === "true" && process.env.NODE_ENV !== "production") return;
+        //if (process.env.REACT_APP_DEBUGG === "true" && process.env.NODE_ENV !== "production") return;
 
         // Return if the parent is not defined
         if (!parentRef.current) return;
@@ -138,81 +121,15 @@ const Background = memo(({ parent }) => {
         // Return if mose is outside the splashscreen div
         if (mouseX < 0 || mouseX > parentWidth || mouseY < 0 || mouseY > parentHeight) return;
 
-        // Normalize position [-1, 1]
+        // Normalize position [0, 1]
         const normX = invlerp(0, parentWidth, mouseX) * 2 - 1;
         const normY = invlerp(0, parentHeight, mouseY) * 2 - 1;
 
-        // Set the motion
-        motion.current = {
-            alpha: normY * 100,
-            beta: normX * 100,
-        };
-    };
+        // Current background position
+        const backgroundPos = backgroundPosition.get().replace(" ", "").split("px");
+        const backgroundCoords = { x: parseInt(backgroundPos[0]), y: parseInt(backgroundPos[1]) };
 
-    // #################################################
-    //   LOOP
-    // #################################################
-
-    const frameID = useRef(0);
-    const frameCount = useRef(0);
-    const lastFrameTime = useRef(Date.now());
-
-    // Update actions
-    const update = (deltaTime) => {
-        // Frame deceleration
-        const frameDesceleration = DECELERATION * deltaTime;
-
-        // New speed same as old
-        var newSpeed = { x: speed.get().x, y: speed.get().y };
-
-        // Update x speed
-        if (prevMotion.current.beta !== motion.current.beta) newSpeed.x = motion.current.beta;
-        else if (newSpeed.x !== 0) newSpeed.x = newSpeed.x > 0 ? Math.max(newSpeed.x - frameDesceleration, 0) : Math.min(newSpeed.x + frameDesceleration, 0);
-
-        // Update y speed
-        if (prevMotion.current.alpha !== motion.current.alpha) newSpeed.y = motion.current.alpha;
-        else if (newSpeed.y !== 0) newSpeed.y = newSpeed.y > 0 ? Math.max(newSpeed.y - frameDesceleration, 0) : Math.min(newSpeed.y + frameDesceleration, 0);
-
-        // Set speed and save current motion
-        if (prevMotion.current.beta !== motion.current.beta || prevMotion.current.alpha !== motion.current.alpha) prevMotion.current = motion.current;
-
-        // Update speed
-        if (newSpeed.x !== speedRef.current.x || newSpeed.y !== speedRef.current.y) {
-            setSpeed({ speed: newSpeed });
-            speedRef.current = newSpeed;
-        }
-
-        // Get te new position
-        var newPosition = {
-            x: positionRef.current.x + speed.get().x * deltaTime,
-            y: positionRef.current.y + speed.get().y * deltaTime,
-        };
-
-        // Update position
-        if (newPosition.x !== positionRef.current.x || newPosition.y !== positionRef.current.y) {
-            setPosition(newPosition);
-            positionRef.current = newPosition;
-        }
-
-        // Update Gradient
-        if (prevGradient !== background.get()) setPrevGradient(background.get());
-    };
-
-    // Called every frame
-    const loop = () => {
-        frameCount.current++;
-
-        if (frameCount.current >= Math.round(MAX_FPS / FPS)) {
-            // Get delta time
-            var currentTime = Date.now();
-            var deltaTime = (currentTime - lastFrameTime.current) / 1000;
-            lastFrameTime.current = currentTime;
-
-            // Update
-            update(deltaTime);
-            frameCount.current = 0;
-        }
-        frameID.current = window.requestAnimationFrame(loop);
+        setPosition({ backgroundPosition: `${backgroundCoords.x - normX * 1000}px ${backgroundCoords.y - normY * 1000}px` });
     };
 
     // #######################################
@@ -222,23 +139,15 @@ const Background = memo(({ parent }) => {
     // On componente mount
     useEffect(() => {
         // Subscribe to events
-        window.addEventListener("resize", onResize);
         if (isMobileOnly) window.addEventListener("devicemotion", onDeviceMotion, true);
         else window.addEventListener("mousemove", onMouseMove, true);
         window.PubSub.sub("onSectionChange", onSectionChange);
 
-        // Start loop
-        loop();
-
         // Unsubscribe from events and stop loop
         return () => {
-            window.removeEventListener("resize", onResize);
             window.removeEventListener("devicemotion", onDeviceMotion, true);
             window.removeEventListener("mousemove", onMouseMove, true);
             window.PubSub.unsub("onSectionChange", onSectionChange);
-
-            // Cancel animation
-            if (frameID.current) window.cancelAnimationFrame(frameID.current);
         };
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -248,58 +157,12 @@ const Background = memo(({ parent }) => {
     //   RENDER
     // #################################################
 
-    // No tiles if the parent is null
-    if (!parent) {
-        var tiles = [];
-    }
-
-    // Tiles to cover only the parent
-    else {
-        // Constants
-        const PARENT_DIMENSIONS = parent.getBoundingClientRect();
-        const SCREEN_WIDTH = PARENT_DIMENSIONS.width;
-        const SCREEN_HEIGHT = PARENT_DIMENSIONS.height;
-        const TILE_SIZE = PATTERNS[currPattern].tileSizeMobile; //SCREEN_WIDTH / PATTERNS[currPattern].tilesPhone;
-        const NUM_TILES = { x: Math.ceil(SCREEN_WIDTH / TILE_SIZE) + 2, y: Math.ceil(SCREEN_HEIGHT / TILE_SIZE) + 2 };
-        const TILE_SIZE_DESKTOP = PATTERNS[currPattern].tileSize; //SCREEN_WIDTH / PATTERNS[currPattern].tiles;
-        const NUM_TILES_DESKTOP = { x: Math.ceil(SCREEN_WIDTH / TILE_SIZE_DESKTOP) + 2, y: Math.ceil(SCREEN_HEIGHT / TILE_SIZE_DESKTOP) + 2 };
-        const isMobile = window.innerWidth < COLLAPSE_NAVBAR_WIDTH;
-
-        // Tile matrix
-        var numTiles = isMobile ? NUM_TILES : NUM_TILES_DESKTOP;
-        var tileSize = isMobile ? TILE_SIZE : TILE_SIZE_DESKTOP;
-
-        tiles = [];
-        var xSize = numTiles.x * tileSize;
-        var ySize = numTiles.y * tileSize;
-        for (let i = 0; i < numTiles.x; i++) {
-            for (let j = 0; j < numTiles.y; j++) {
-                // Get X position
-                var displX = position.x + i * tileSize;
-                if (displX > 0) var xPos = (displX % xSize) - tileSize;
-                else xPos = tileSize * (numTiles.x - 1) - (Math.abs(displX) % xSize);
-
-                // Get Y position
-                var displY = position.y + j * tileSize;
-                if (displY > 0) var yPos = (displY % ySize) - tileSize;
-                else yPos = tileSize * (numTiles.y - 1) - (Math.abs(displY) % ySize);
-
-                tiles.push(
-                    <SVG
-                        key={`${i - 1}-${j - 1}`}
-                        className={classnames("cell", currPattern)}
-                        src={PATTERNS[currPattern].svg}
-                        style={{ width: tileSize, transform: `translate(${xPos}px, ${yPos}px)` }}
-                    />
-                );
-            }
-        }
-    }
-
     return (
-        <div className="background" style={{ backgroundImage: background.get() }}>
-            {tiles}
-        </div>
+        <animated.div className="background" style={{ backgroundImage }}>
+            <animated.div className="webPattern" style={{ backgroundPosition, opacity: opacities.opacityWeb }}></animated.div>
+            <animated.div className="gamePattern" style={{ backgroundPosition, opacity: opacities.opacityGame }}></animated.div>
+            <animated.div className="designPattern" style={{ backgroundPosition, opacity: opacities.opacityDesign }}></animated.div>
+        </animated.div>
     );
 });
 
--- src/contexts/Data.jsx
+++ src/contexts/Data.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useRef } from "react";
+import React, { createContext, useState } from "react";
 
 // Data Context
 export const Data = createContext();
@@ -7,24 +7,12 @@ const DataProvider = (props) => {
     // LANDING CHECK
     const [landingDone, setLandingDone] = useState(true); // ROJAS change to false
 
-    // BACKGROUND POSITION
-    const positionRef = useRef({ x: 0, y: 0 });
-    const speedRef = useRef({ x: 0, y: 0 });
-    const motion = useRef({ alpha: 0, beta: 0 });
-    const prevMotion = useRef({ alpha: 0, beta: 0 });
-
     return (
         <Data.Provider
             value={{
                 // LANDING CHECK
                 landingDone,
                 setLandingDone,
-
-                // BACKGROUND POSITION
-                positionRef,
-                speedRef,
-                motion,
-                prevMotion,
             }}
         >
             {props.children}
